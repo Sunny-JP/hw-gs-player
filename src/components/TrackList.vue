5ce1e96e81681d8ecec6c6bbf41a34288af98bc1
Sunny <kajimalu10@gmail.com>
2025-03-24 10:32:08 +0900

    add tracklist

---
+++ src/components/TrackList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="page">
+    <h1>Tracklist</h1>
+    <table>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Track1</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Track2</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Track3</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>Track4</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+.page {
+  position: absolute;
+  top: 0;
+  padding: 10px;
+  z-index: 0;
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 30px;
+  background: #000000;
+}
+</style>
--- src/router.ts
+++ src/router.ts
@@ -2,19 +2,21 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Top from "./components/AlbumList.vue";
 import About from "./components/About.vue";
 import Enquiries from './components/Enquiries.vue';
+import FavoriteList from './components/FavoriteList.vue';
 import Info from './components/Info.vue';
 import Policy from './components/Policy.vue';
 import Terms from './components/Terms.vue';
-import FavoriteList from './components/FavoriteList.vue';
+import TrackList from './components/TrackList.vue';
 
 const routes = [
   { path: '/', name: 'top', component: Top },
   { path: '/about', name: 'about', component: About },
   { path: '/enquiries', name: 'enquiries', component: Enquiries },
+  { path: '/favorite', name: 'favorite', component: FavoriteList },
   { path: '/info', name: 'info', component: Info },
   { path: '/policy', name: 'policy', component: Policy },
   { path: '/terms', name: 'terms', component: Terms },
-  {path: '/favorite', name: 'favorite', component: FavoriteList},
+  { path: '/tracklist', name: 'tracklist', component: TrackList },
 ]
 
 const router = createRouter({
